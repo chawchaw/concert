30f87fa0ae41580841c31fa9faa2a6baf0007904
chaw <d.chaow@gmail.com>
2024-10-25 03:04:59 +0900

    기존 E2E 테스트 삭제

--- src/test/java/com/chaw/app/domain/concert/e2e/ConcertE2E.java
+++ src/test/java/com/chaw/app/domain/concert/e2e/ConcertE2E.java
@@ -5,43 +5,24 @@ import com.chaw.concert.app.domain.common.auth.entity.User;
 import com.chaw.concert.app.domain.common.auth.respository.UserRepository;
 import com.chaw.concert.app.domain.common.user.repository.PointHistoryRepository;
 import com.chaw.concert.app.domain.common.user.repository.PointRepository;
-import com.chaw.concert.app.domain.common.user.usecase.ChargePoint;
 import com.chaw.concert.app.domain.concert.query.entity.*;
 import com.chaw.concert.app.domain.concert.query.repository.ConcertRepository;
 import com.chaw.concert.app.domain.concert.query.repository.ConcertScheduleRepository;
 import com.chaw.concert.app.domain.concert.query.repository.TicketRepository;
-import com.chaw.concert.app.domain.concert.query.usecase.GetConcertSchedulesNotSoldOut;
-import com.chaw.concert.app.domain.concert.query.usecase.GetConcerts;
-import com.chaw.concert.app.domain.concert.query.usecase.GetTicketsInEmptyStatus;
-import com.chaw.concert.app.domain.concert.queue.entity.WaitQueueStatus;
 import com.chaw.concert.app.domain.concert.queue.repository.WaitQueueRepository;
 import com.chaw.concert.app.domain.concert.queue.scheduler.PassWaitQueue;
-import com.chaw.concert.app.domain.concert.queue.usecase.EnterWaitQueue;
 import com.chaw.concert.app.domain.concert.reserve.repository.PaymentRepository;
 import com.chaw.concert.app.domain.concert.reserve.repository.ReserveRepository;
-import com.chaw.concert.app.domain.concert.reserve.usecase.PayTicket;
-import com.chaw.concert.app.domain.concert.reserve.usecase.RequestReserve;
-import com.chaw.concert.app.presenter.controller.api.v1.user.dto.ChargePointInput;
 import com.chaw.helper.DatabaseCleanupListener;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
-import java.text.MessageFormat;
 import java.time.LocalDateTime;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest(classes = ConcertApplication.class)
 @ExtendWith(SpringExtension.class)
@@ -123,103 +104,5 @@ public class ConcertE2E {
         ticketRepository.save(ticket);
     }
 
-    /**
-     * 대기열 입장 -> 통과 -> 콘서트조회 -> 일정조회 -> 티켓조회 -> 예약 -> 결제(잔액실패) -> 포인트 충전 -> 결제(성공)
-     */
-    @Test
-    @Disabled // 로컬 서버 실행 필요
-    void success_pay() {
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("uuid", uuid);
-
-        // 대기열 입장
-        ResponseEntity<EnterWaitQueue.Output> enterWaitQueueResult = restTemplate.exchange(
-                MessageFormat.format("{0}/concert/queue", host),
-                HttpMethod.POST,
-                new HttpEntity<>(headers),
-                EnterWaitQueue.Output.class
-        );
-        assertEquals(WaitQueueStatus.WAIT.name(), enterWaitQueueResult.getBody().status());
-
-        // 통과
-        passWaitQueue.execute();
-        ResponseEntity<EnterWaitQueue.Output> enterWaitQueueResultAfterPass = restTemplate.exchange(
-                MessageFormat.format("{0}/concert/queue", host),
-                HttpMethod.POST,
-                new HttpEntity<>(headers),
-                EnterWaitQueue.Output.class
-        );
-        assertEquals(WaitQueueStatus.PASS.name(), enterWaitQueueResultAfterPass.getBody().status());
-
-        // 콘서트조회
-        ResponseEntity<GetConcerts.Output> getConcertsResult = restTemplate.exchange(
-                MessageFormat.format("{0}/concert", host),
-                HttpMethod.GET,
-                new HttpEntity<>(headers),
-                GetConcerts.Output.class
-        );
-        assertEquals(1, getConcertsResult.getBody().concerts().size());
-        GetConcerts.Output.ConcertOutput concert = getConcertsResult.getBody().concerts().get(0);
-
-        // 일정조회
-        ResponseEntity<GetConcertSchedulesNotSoldOut.Output> getSchedulesResult = restTemplate.exchange(
-                MessageFormat.format("{0}/concert/{1}/schedule", host, concert.id()),
-                HttpMethod.GET,
-                new HttpEntity<>(headers),
-                GetConcertSchedulesNotSoldOut.Output.class
-        );
-        assertEquals(1, getSchedulesResult.getBody().schedules().size());
-        GetConcertSchedulesNotSoldOut.Output.Item concertSchedule = getSchedulesResult.getBody().schedules().get(0);
-
-        // 티켓조회
-        ResponseEntity<GetTicketsInEmptyStatus.Output> getTicketsResult = restTemplate.exchange(
-                MessageFormat.format("{0}/concert/{1}/schedule/{2}/tickets", host, concert.id(), concertSchedule.id()),
-                HttpMethod.GET,
-                new HttpEntity<>(headers),
-                GetTicketsInEmptyStatus.Output.class
-        );
-        assertEquals(1, getTicketsResult.getBody().tickets().size());
-        GetTicketsInEmptyStatus.Output.Item ticket = getTicketsResult.getBody().tickets().get(0);
-
-        // 예약
-        ResponseEntity<RequestReserve.Output> reserveResult = restTemplate.exchange(
-                MessageFormat.format("{0}/concert/{1}/schedule/{2}/tickets/{3}/reserve", host, concert.id(), concertSchedule.id(), ticket.id()),
-                HttpMethod.POST,
-                new HttpEntity<>(headers),
-                RequestReserve.Output.class
-        );
-        assertEquals(true, reserveResult.getBody().success());
-
-        // 결제(잔액실패)
-        try {
-            restTemplate.exchange(
-                    MessageFormat.format("{0}/concert/{1}/schedule/{2}/tickets/{3}/pay", host, concert.id(), concertSchedule.id(), ticket.id()),
-                    HttpMethod.POST,
-                    new HttpEntity<>(headers),
-                    PayTicket.Output.class
-            );
-        } catch (HttpClientErrorException e) {
-            assertEquals(404, e.getRawStatusCode());
-        }
-
-        // 포인트 충전
-        ChargePointInput chargePointInput = new ChargePointInput(100000);
-        ResponseEntity<ChargePoint.Output> chargePointResult = restTemplate.exchange(
-                MessageFormat.format("{0}/user/point/charge", host),
-                HttpMethod.POST,
-                new HttpEntity<>(chargePointInput, headers),
-                ChargePoint.Output.class
-        );
-        assertEquals(chargePointInput.point(), chargePointResult.getBody().balance());
-
-        // 결제(성공)
-        ResponseEntity<PayTicket.Output> payResult = restTemplate.exchange(
-                MessageFormat.format("{0}/concert/{1}/schedule/{2}/tickets/{3}/pay", host, concert.id(), concertSchedule.id(), ticket.id()),
-                HttpMethod.POST,
-                new HttpEntity<>(headers),
-                PayTicket.Output.class
-        );
-        assertEquals(true, payResult.getBody().success());
-    }
 
 }
