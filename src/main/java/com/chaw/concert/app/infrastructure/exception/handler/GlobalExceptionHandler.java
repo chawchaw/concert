6ab09141585c7a01a3fb0ff0acf7fac7873d2425
chaw <d.chaow@gmail.com>
2024-10-27 21:51:44 +0900

    Refactor: ExceptionHandler 에서 HttpServletRequest 를 사용하여 로깅정보 추가

--- src/main/java/com/chaw/concert/app/infrastructure/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/chaw/concert/app/infrastructure/exception/handler/GlobalExceptionHandler.java
@@ -2,12 +2,15 @@ package com.chaw.concert.app.infrastructure.exception.handler;
 
 import com.chaw.concert.app.infrastructure.exception.common.BaseException;
 import com.chaw.concert.app.infrastructure.slack.SlackNotifierService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.text.MessageFormat;
 
 @RestControllerAdvice
 @Slf4j
@@ -20,25 +23,27 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     }
 
     @ExceptionHandler(BaseException.class)
-    public ResponseEntity<ExceptionResponse> handleBaseException(BaseException ex) {
+    public ResponseEntity<ExceptionResponse> handleBaseException(BaseException ex, HttpServletRequest request) {
         HttpStatus status = ex.getErrorType().getHttpStatus();
         if (status == HttpStatus.INTERNAL_SERVER_ERROR) {
-            logInternalServerErrors(ex);
+            logInternalServerErrors(ex, request);
         }
 
         return new ResponseEntity<>(new ExceptionResponse(ex.getMessage(), status), status);
     }
 
     @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<ExceptionResponse> handleRuntimeException(RuntimeException ex) {
-        logInternalServerErrors(ex);
+    public ResponseEntity<ExceptionResponse> handleRuntimeException(RuntimeException ex, HttpServletRequest request) {
+        logInternalServerErrors(ex, request);
         HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
         return new ResponseEntity<>(new ExceptionResponse(ex.getMessage(), status), status);
     }
 
-    public void logInternalServerErrors(RuntimeException ex) {
-        log.error(HttpStatus.INTERNAL_SERVER_ERROR.name(), ex);
-        slackNotifierService.sendErrorNotificationToSlack(ex.getMessage());
+    public void logInternalServerErrors(RuntimeException ex, HttpServletRequest request) {
+        String requestInfo = MessageFormat.format("({0}) [{1}] {2}", request.getRemoteAddr(), request.getMethod(), request.getRequestURI());
+        String message = MessageFormat.format("{0} - {1} | Request: {}", HttpStatus.INTERNAL_SERVER_ERROR.name(), ex.getMessage(), requestInfo);
+        log.error(message, ex);
+        slackNotifierService.sendErrorNotificationToSlack(message);
     }
 
     public record ExceptionResponse(String message, String status, int code) {
